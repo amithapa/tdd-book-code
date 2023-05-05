55b467b817ccd0ce8a1365a600bd645b1f69cf3a
amithapa <amithapa1994@gmail.com>
2023-05-05 13:16:07 +0545

    feat: convert between any currencies with defined exchange rates

--- py/portfolio.py
+++ py/portfolio.py
@@ -12,10 +12,13 @@ class Portfolio:
         self.moneys.extend(moneys)
 
     def __convert(self, a_money: Money, a_currency: str) -> float:
+        exchange_rates = {"EUR->USD": 1.2, "USD->KRW": 1100}
+
         if a_money.currency == a_currency:
             return a_money.amount
         else:
-            return a_money.amount * self._eur_to_usd
+            key = f"{a_money.currency}->{a_currency}"
+            return a_money.amount * exchange_rates[key]
 
     def evaluate(self, currency: str) -> Money:
         total = functools.reduce(
